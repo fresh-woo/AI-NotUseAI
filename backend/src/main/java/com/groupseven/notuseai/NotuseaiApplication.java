d9f0efcc1faf1131f9c2f622cc52bf78827e0bc1
thepackour <thepackour5610@gmail.com>
2025-11-19 19:08:42 +0900

    합치기 완료

---
+++ backend/src/main/java/com/groupseven/notuseai/NotuseaiApplication.java
@@ -0,0 +1,13 @@
+package com.groupseven.notuseai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotuseaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotuseaiApplication.class, args);
+	}
+
+}
